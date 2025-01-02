36993eb28610649447341a8d12b50f4f17be764c
gneiru <noelrohi59@gmail.com>
2025-01-02 17:56:42 +0800

    use unkey to protect route

--- src/app/api/new/route.ts
+++ src/app/api/new/route.ts
@@ -1,6 +1,6 @@
 import { db } from "@/db";
 import { items } from "@/db/schema";
-import { env } from "@/env";
+import { withUnkey } from "@unkey/nextjs";
 import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
 
@@ -12,19 +12,23 @@ const newItemSchema = createInsertSchema(items, {
   updatedAt: true,
 });
 
-export async function POST(req: Request) {
-  const password = req.headers.get("Authorization");
-  if (password !== env.MYPASSWORD) {
-    return Response.json({ error: "Invalid password" }, { status: 401 });
-  }
-  try {
-    const body = await req.json();
-    const parsedBody = newItemSchema.parse(body);
-    const newItem = await db.insert(items).values(parsedBody).returning();
+export const POST = withUnkey(
+  async (req) => {
+    if (!req.unkey?.valid) {
+      return new Response("Unauthorized", { status: 403 });
+    }
+    try {
+      const body = await req.json();
+      const parsedBody = newItemSchema.parse(body);
+      const newItem = await db.insert(items).values(parsedBody).returning();
 
-    return Response.json(newItem);
-  } catch (error) {
-    console.error(error);
-    return Response.json({ error: "Invalid request body" }, { status: 400 });
-  }
-}
+      return Response.json(newItem);
+    } catch (error) {
+      console.error(error);
+      return Response.json({ error: "Invalid request body" }, { status: 400 });
+    }
+  },
+  {
+    apiId: "api_2CuNTn1twmkNK4RntE9W8KTQLmjz",
+  },
+);
--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,6 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
     DATABASE_AUTH_TOKEN: z.string().min(1),
-    MYPASSWORD: z.string().min(1),
     EXA_API_KEY: z.string().min(1),
   },
   client: {
@@ -14,7 +13,6 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     DATABASE_AUTH_TOKEN: process.env.DATABASE_AUTH_TOKEN,
-    MYPASSWORD: process.env.MYPASSWORD,
     EXA_API_KEY: process.env.EXA_API_KEY,
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
