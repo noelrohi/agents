76ba41b7faa1429e8c5601b838ecc088c8858159
gneiru <noelrohi59@gmail.com>
2025-02-19 18:50:54 +0800

    chore: remove react-scan script from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,8 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <script src="https://unpkg.com/react-scan/dist/auto.global.js" />
-        {/* rest of your scripts go under */}
+        {/* <script src="https://unpkg.com/react-scan/dist/auto.global.js" /> */}
       </head>
       <body
         className={`${fontSans.variable} ${fontMono.variable} ${fontHeading.variable} font-sans antialiased`}
